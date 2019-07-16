7d8678346e4f6092c443fce35d63652abc9e3907
Christopher Coronado <christopher.coronado@telenetwork.com>
2019-07-16 18:32:37 -0500

    Update custom style; add drawer

---
+++ src/app/open-kahn-material-module/open-kahn-material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import {MatCardModule, MatSidenavModule} from '@angular/material';
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatCardModule,
+    MatSidenavModule,
+  ],
+})
+export class OpenKahnMaterialModule { }
